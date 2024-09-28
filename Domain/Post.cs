a0b5de1bcc78175e3d8cf56463425778f9a62483
mrdotdisability <mrdotdisability@gmail.com>
2024-09-28 16:25:14 +1200

    Created migration

--- Domain/Post.cs
+++ Domain/Post.cs
@@ -2,7 +2,7 @@
 
 public class Post
 {
-	public string Id { get; set; }
+	public Guid Id { get; set; }
 	public string Title { get; set; }
     public string Body { get; set; }
 	public DateTime CreatedDate { get; set; }
---
+++ Migrations/20240928042153_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Body = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Tag = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
